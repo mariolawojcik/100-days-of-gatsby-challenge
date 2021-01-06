23d4c8aac6e8f37f0c01c4e10a4fbbea51907bee
Mariola <mariolasikora11@gmail.com>
2021-01-06 18:58:19 +0100

    sitemap plugin temporary commented

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    // "gatsby-plugin-sitemap",
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
